7c7d151bcbbeb4514c8cecb1d525f028a29cd0bc
Deep Mehta <55307573+deepmehta922000@users.noreply.github.com>
2026-01-31 14:18:53 -0500

    Create 2041-TheLatestLoginIn2020

---
+++ 2041-TheLatestLoginIn2020/2041-TheLatestLoginIn2020.sql
@@ -0,0 +1,6 @@
+-- Last updated: 1/31/2026, 2:18:51 PM
+# Write your MySQL query statement below
+select user_id, max(time_stamp) 'last_stamp'
+from logins
+where time_stamp like '2020%'
+group by user_id ;
