409e82f06c22d75c8a9bedb5a0f7633b50b2c594
Deep Mehta <55307573+deepmehta922000@users.noreply.github.com>
2026-01-31 14:18:30 -0500

    Create 3501-DeleteNodesFromLinkedListPresentInArray

---
+++ 3501-DeleteNodesFromLinkedListPresentInArray/3501-DeleteNodesFromLinkedListPresentInArray.py
@@ -0,0 +1,28 @@
+# Last updated: 1/31/2026, 2:18:29 PM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
+
+        values_to_remove = set(nums)
+
+        while head and head.val in values_to_remove:
+            head = head.next
+        
+        if not head:
+            return None
+        
+        current = head
+
+        while current.next:
+            if current.next.val in values_to_remove:
+                current.next = current.next.next
+            
+            else:
+                current = current.next
+            
+        return head
+        
